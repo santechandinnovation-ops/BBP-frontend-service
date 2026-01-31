90d678e0e27340c4efa7f8df9fb92aaf81a2abf4
BusNavetta <francesco7.nava@mail.polimi.it>
2026-01-31 18:25:24 +0100

    .

--- app/config/settings.py
+++ app/config/settings.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Settings:
     API_GATEWAY_URL: str = os.getenv("API_GATEWAY_URL", "http://localhost:8080")
-    JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY", "bbp-secret-key-2024")
+    JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY", "bbp-secret-key-2025")
     PORT: int = int(os.getenv("PORT", "8004"))
 
 settings = Settings()
